7adb96fc109f960f23ba573f3e96ba219967b03e
kamil-matyja <matyja_kamil@o2.pl>
2026-04-13 20:54:27 +0200

    Change chmod for project

--- backend/tests/Integration/FactorySmokeTest.php
+++ backend/tests/Integration/FactorySmokeTest.php
@@ -2,11 +2,45 @@
 
 declare(strict_types=1);
 
-namespace Tests\Factory;
+namespace Tests\Integration;
 
 use Generator;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
+use Tests\Factory\ConversationActivityFactory;
+use Tests\Factory\ConversationFactory;
+use Tests\Factory\EntryFactory;
+use Tests\Factory\EventDisciplineDistanceFactory;
+use Tests\Factory\EventDisciplineFactory;
+use Tests\Factory\EventDisciplineListFactory;
+use Tests\Factory\EventDisciplineResultFactory;
+use Tests\Factory\EventDisciplineSubDistanceFactory;
+use Tests\Factory\EventDisciplineSubResultFactory;
+use Tests\Factory\EventFactory;
+use Tests\Factory\FeedCommentFactory;
+use Tests\Factory\FeedFactory;
+use Tests\Factory\FeedReactionFactory;
+use Tests\Factory\FriendFactory;
+use Tests\Factory\GoalFactory;
+use Tests\Factory\GoalParticipantFactory;
+use Tests\Factory\GoalParticipantResultFactory;
+use Tests\Factory\NotificationFactory;
+use Tests\Factory\PageFactory;
+use Tests\Factory\PageFollowFactory;
+use Tests\Factory\PageParticipantFactory;
+use Tests\Factory\PushSubscriptionFactory;
+use Tests\Factory\StoryFactory;
+use Tests\Factory\TrainingDisciplineDistanceFactory;
+use Tests\Factory\TrainingDisciplineFactory;
+use Tests\Factory\TrainingDisciplineSubDistanceFactory;
+use Tests\Factory\TrainingFactory;
+use Tests\Factory\TrainingParticipantFactory;
+use Tests\Factory\UserDisciplineFactory;
+use Tests\Factory\UserFactory;
+use Tests\Factory\UserPasswordResetFactory;
+use Tests\Factory\UserRegisterFactory;
+use Tests\Factory\UserRoleFactory;
+use Tests\Factory\UserSignFactory;
 
 class FactorySmokeTest extends TestCase
 {
@@ -59,3 +93,4 @@ class FactorySmokeTest extends TestCase
         yield [UserSignFactory::class];
     }
 }
+
