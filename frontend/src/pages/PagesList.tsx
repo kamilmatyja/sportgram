78e9a288e9bdac8d1c459d3a369c4bafdb1a5153
kamil-matyja <matyja_kamil@o2.pl>
2026-05-26 12:36:06 +0200

    Add page for pages

--- frontend/src/pages/PagesList.tsx
+++ frontend/src/pages/PagesList.tsx
@@ -1,4 +1,44 @@
+import {usePages} from '../services/usePages';
+import {usePageModals} from '../services/usePageModals';
+import {PagesListView} from '../components/PagesListView';
+import {AddPageModal} from '../components/AddPageModal';
+
 export default function PagesList() {
-    return <div className="container mt-5"><h2>Lista Stron</h2><p className="text-muted">Widok wszystkich stron.
-        Przyciski do zaobserwowania.</p></div>;
+    const pagesService = usePages();
+    const modalsService = usePageModals(pagesService.fetchPages);
+
+    return (
+        <>
+            <PagesListView
+                pages={pagesService.pages}
+                isOrganizer={pagesService.isOrganizer}
+                loading={pagesService.loading}
+                error={pagesService.error}
+                page={pagesService.page}
+                limit={pagesService.limit}
+                sort={pagesService.sort}
+                filters={pagesService.filters}
+                onFilterChange={pagesService.handleFilterChange}
+                onSortChange={pagesService.handleSortChange}
+                onLimitChange={pagesService.handleLimitChange}
+                onPrevPage={pagesService.handlePrevPage}
+                onNextPage={pagesService.handleNextPage}
+                onAddClick={modalsService.openAddModal}
+            />
+
+            <AddPageModal
+                user={pagesService.currentUser}
+                show={modalsService.showAdd}
+                availableUsers={modalsService.availableUsers}
+                closeModal={modalsService.closeAddModal}
+                loading={modalsService.loading}
+                globalError={modalsService.globalError}
+                fieldErrors={modalsService.fieldErrors}
+                formData={modalsService.formData}
+                handleChange={modalsService.handleChange}
+                handleParticipantsChange={modalsService.handleParticipantsChange}
+                handleSubmit={modalsService.handleAddSubmit}
+            />
+        </>
+    );
 }
